9ae2f43b6e28fd66c8c4739f0c54925cbe840d92
Nova Boman <nova.boman@chasacademy.se>
2022-04-21 22:49:23 +0200

    Add html template

--- src/components/Current.js
+++ src/components/Current.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Current = () => {
-  return <div className="current">Current</div>;
+  return (
+    <div className="current">
+      <h2 className="location">Location</h2>
+      <p>Day | Date </p>
+      <p className="temperature">23C</p>
+      <p>Sunny</p>
+      <div className="short-info">
+        <p>Wind</p>
+        <p>Icon</p>
+        <p>Nederb.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Current;
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,7 +1,53 @@
 import React from "react";
 
 const Forecast = () => {
-  return <div className="forecast">Forecast</div>;
+  return (
+    <div className="forecast">
+      <p>Forecast</p>
+      <div className="overview">
+        <div className="overview-card">Mon.</div>
+        <div className="overview-card">Tue.</div>
+        <div className="overview-card">wed.</div>
+        <div className="overview-card">Thu.</div>
+        <div className="overview-card">Fri.</div>
+      </div>
+      <p>Detail</p>
+      <div className="detail">
+        <div className="hourly">
+          <table>
+            <thead>
+              <tr>
+                <th scope="col">Time</th>
+                <th scope="col">Temp</th>
+                <th scope="col">Wind</th>
+                <th scope="col">Prec.</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>12</td>
+                <td>15</td>
+                <td>4</td>
+                <td>0mm</td>
+              </tr>
+              <tr>
+                <td>13</td>
+                <td>15</td>
+                <td>3</td>
+                <td>0mm</td>
+              </tr>
+              <tr>
+                <td>14</td>
+                <td>17</td>
+                <td>2</td>
+                <td>0mm</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Forecast;
