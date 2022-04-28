3effcac42ca47e9907fa0e9c0d57c63a76350f91
Nova Boman <nova.boman@chasacademy.se>
2022-04-28 19:32:53 +0200

    Create helper functions for date formats

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -5,3 +5,20 @@ export const toDate = (timestamp) => {
   const date = dateObject.toLocaleString();
   return date;
 };
+
+// Format date
+export const formatDate = (timestamp, option = "long") => {
+  const date = toDateObject(timestamp);
+  return date.toLocaleDateString("en-GB", {
+    weekday: option,
+    month: option,
+    day: "numeric",
+  });
+};
+
+// Convert Unix timestamp to Date object
+export const toDateObject = (timestamp) => {
+  const ms = timestamp * 1000;
+  const dateObject = new Date(ms);
+  return dateObject;
+};
